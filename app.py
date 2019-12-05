c9030199203bac2eb9eef6628867c6ff87381fc0
Sri-R <Sri-R@users.noreply.github.com>
2019-12-04 19:07:59 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, url_for
+app = Flask(__name__)
+
+
+version = open("version", "r")
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+@app.route('/version')
+def api_version():
+    return 'Version is ' + version.read()
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+@app.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading ' + articleid
+
+if __name__ == '__main__':
+    app.run()
